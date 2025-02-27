b9e1dc7eccb0e9f738388cf00dcffa25259f8160
Beatriz Veloso <beatrizoslov6@gmail.com>
2025-02-27 09:41:34 -0300

    Update aula-27.02.2025.java

--- aula-27.02.2025.java
+++ aula-27.02.2025.java
@@ -1,4 +1,9 @@
 /*Prof Jeffersson- Exercícios do Blackboard*/
+
+
+/*Exercício 1 - Elaborar um algoritmo que solicite os dados de 2 retângulos para calcular e visualizar três áreas: AT (área total das duas figuras), A1 e A2 (áreas dos retângulos superior e inferior). Os únicos dados 
+conhecidos são os valores a, b, d.*/
+
 package Aula2702;
 import java.util.Scanner;
 public class Exer1 {
@@ -22,6 +27,22 @@ public class Exer1 {
     }
     static int A2(int b, int d) {
         return b * d;
+    }
+}
+
+/*Exercício 2 - Elabore um algoritmo que leia dois números inteiros e imprima o resto da divisão inteira de um pelo outro*/
+
+package Aula2702;
+import java.util.Scanner;
+public class RestoDivisao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+        int resto = num1 % num2;
+        System.out.println("O resto da divisão inteira de " + num1 + " por " + num2 + " é: " + resto);
     }
 }
 
