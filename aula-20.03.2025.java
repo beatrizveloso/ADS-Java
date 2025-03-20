2b303371fd48259fb4c5c22616adb7d3a348c50c
Beatriz Veloso <beatrizoslov6@gmail.com>
2025-03-20 09:32:43 -0300

    Update aula-20.03.2025.java

--- aula-20.03.2025.java
+++ aula-20.03.2025.java
@@ -4,31 +4,32 @@ import java.util.Scanner;
 public class ExemploA {
     public static void main(String args[]){
         Scanner objScan = new Scanner(System.in);
+        ExemploB objExecB = new ExemploB();
         int n1, n2, soma;
         System.out.println("INf. n1 ");
         n1 = objScan.nextInt();
         System.out.println("INf. n2 ");
         n2 = objScan.nextInt();
-        soma = Somar(n1, n2);
+        objExecB.rec(n1, n2);
+        soma = objExecB.Somar();
         System.out.println("A soma foi " + soma);
     }
-    static int Somar(int n1, int n2) {
-        return n1 + n2;
-    }
 }
+
 
 /*Exemplo B*/
 package Aula2003;
 public class ExemploB {
-    int num1;
-    int num2;
+    private int num1;
+    private int num2;
 
-    void rec(int num1, int num2) {
+    public void rec(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
     }
 
-    int Somar() {
-        return this.num1 + num2;
+    public int Somar() {
+        return this.num1 + this.num2;
 }
 }
+
