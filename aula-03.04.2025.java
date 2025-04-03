a0cb2b2f8d215f80040466829b76e65053dfb7db
Beatriz Veloso <beatrizoslov6@gmail.com>
2025-04-03 09:09:01 -0300

    Create aula-03.04.2025.java

---
+++ aula-03.04.2025.java
@@ -0,0 +1,21 @@
+/*Aula Jefferson - Estrutura de repetição*/
+package Aula0304;
+
+public class exemplo {
+
+    public static void main(String args[]) {
+        int cont = 1;
+
+        /*while (cont <= 10) {
+            System.out.println("Número " + cont);
+            cont++;
+        }
+        do {
+        System.out.println("Número " + cont);
+        cont++;
+    } while (cont <= 10);
+        */
+        for (cont = 1; cont <= 10; cont++)
+         System.out.println("Número " + cont);
+    }
+}
